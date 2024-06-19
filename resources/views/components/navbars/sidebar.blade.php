cf09e003c72224cbb73535b48615d722da57f071
ayeye's servant <sami287borda@gmail.com>
2024-06-19 01:56:36 -0400

    dashboardupdate

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -1,6 +1,6 @@
 @props(['activePage'])
 
-<aside class="sidenav navbar navbar-vertical navbar-expand-xs bg-gray-450" id="sidenav-main">
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs    fixed-start  custom-max-width bg-gray-450" id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
