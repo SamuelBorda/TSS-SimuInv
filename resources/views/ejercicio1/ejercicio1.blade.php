07fde0470a0d52b74bfbc2a66986a084aeb6dbaf
MadaiFlores <madaifloresmodragon@gmail.com>
2024-06-20 17:33:56 -0400

    colores botones ver simular

--- resources/views/ejercicio1/ejercicio1.blade.php
+++ resources/views/ejercicio1/ejercicio1.blade.php
@@ -12,7 +12,7 @@
             <div class="panelTitulo">
                 <div class="tituloEjercicio">
                     <h3><span class="bg-gradient-titleejercicio">
-                        CASO 1: COMPARACIÓN DE POLITICAS DE INVENTARIO
+                        CASO 1 - COMPARACIÓN DE POLITICAS DE INVENTARIO
                     </span></h3>
                 </div>
                 <div class="botonayuda">
@@ -34,10 +34,10 @@
             </div>
             <div class="botonesEjercicio1 marginIzqDer">
                 <div class="botonverhistorial">
-                    <a href="#" class="btn btn-gradient-outline">Ver historial</a>
+                    <a href="#" class="btn btn-gradienthistorial">Ver historial</a>
                 </div>
                 <div class="botonSimular">
-                    <a href="#" class="btn btn-gradient-outline">Simular</a>
+                    <a href="#" class="btn btn-gradientsimular">Simular</a>
                 </div>
             </div>
         </div>
