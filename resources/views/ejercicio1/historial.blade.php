3aa21ffda65728ebfed8d466134ea4c05ff9a2d0
MadaiFlores <madaifloresmodragon@gmail.com>
2024-06-20 18:09:28 -0400

    rutas acciones ejercicio 1

---
+++ resources/views/ejercicio1/historial.blade.php
@@ -0,0 +1,32 @@
+<x-layout bodyClass="g-sidenav-show  bg-gray-400">
+    <x-navbars.sidebar activePage='dashboard'></x-navbars.sidebar>
+    <main class="main-content position-relative max-height-vh-100 h-100   bg-gray-450"  style="margin-left: 15.625rem;">
+        <!-- Navbar -->
+        <x-navbars.navs.auth titlePage="Dashboard"></x-navbars.navs.auth>
+        <!-- End Navbar -->
+         
+        <br>
+        <br>
+        <br>
+        <div class="py-4  bg-gray-400 text-white">
+            <div>
+                <p>esto es un prueba para ver si me esta mostrando el contenido de historial ejercicio 1</p>
+            </div>
+        </div>
+        
+        </main>
+
+@push('js')
+
+
+
+
+ <!-- Initialize Flatpickr -->
+ <script type="module">
+
+
+
+
+    </script>
+@endpush
+</x-layout>
