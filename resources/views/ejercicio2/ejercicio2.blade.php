3712e16e6a096ac3fbfcb93629a27f3d06b51f45
ayeye's servant <sami287borda@gmail.com>
2024-06-20 23:36:43 -0400

    calculoPolitica1

--- resources/views/ejercicio2/ejercicio2.blade.php
+++ resources/views/ejercicio2/ejercicio2.blade.php
@@ -8,14 +8,14 @@
         <br>
         <br>
         <br>
-        <div class="container-fluid py-4 bg-gray-400"> <!-- solo para el espacio --> <br>   </div>  
+        <div class="container-fluid py-4 bg-gray-400"> <!-- solo para el espacio --> <br>   </div>
 
         <div class="container-fluid py-4 bg-dark-400 text-white">
             <h3 class="text-start ps-5">
                 <span class="bg-gradient-simu px-4 py-2">
                     CASO 2 - MANTENIMIENTO DE EQUIPOS
                 </span>
-                <button type="button" class="gradient-button ms-10" onclick="window.location.href='{{ route('ejercicio2.ayuda') }}'">
+                <button type="button" class="gradient-button ms-10" onclick="window.location.href='{{ route('ejercicio2.ayuda') }}'>
                     <span class="d-none d-md-inline">¿Necesitas ayuda?</span>
                     <span class="d-inline d-md-none">
                         <i class="fas fa-question-circle"></i>
@@ -23,74 +23,74 @@
                 </button>
 
             </h3>
-        </div>  
+        </div>
 
         <div class="container-fluid py-4 bg-gray-400 text-white d-flex " style="min-height: 500px;">
             <div class="text-justify">
                    <br> <br>
                    <p>
                    Una compañía tiene un problema de mantenimiento con cierto equipo,
-                    que contiene 4  componentes electrónicos idénticos que son la causa del mismo, 
-                    el cual consiste en que los  componentes fallan frecuentemente forzando a que el 
-                    equipo se desconecte mientras se hace la  reposición. Lo que se ha venido haciendo es 
+                    que contiene 4  componentes electrónicos idénticos que son la causa del mismo,
+                    el cual consiste en que los  componentes fallan frecuentemente forzando a que el
+                    equipo se desconecte mientras se hace la  reposición. Lo que se ha venido haciendo es
                     reemplazar los componentes solamente cuando se  descomponen. Sin embargo, existe una
-                     nueva proposición de hacer el reemplazo de los cuatro  componentes cuando falle 
+                     nueva proposición de hacer el reemplazo de los cuatro  componentes cuando falle
                      cualesquiera de ellos, con objeto de reducir la frecuencia de desconexión  del equipo.
                     </p>
                  <br>
                 <p>
                 El tiempo de vida de un componente esta normalmente distribuido con media de 600 horas y
-                  desviación estándar de 100 horas. También se sabe que es necesario desconectar el equipo 
+                  desviación estándar de 100 horas. También se sabe que es necesario desconectar el equipo
                   1 hora  si se reemplaza un componente y 2 horas si se reemplazan los 4. Un componente
                    nuevo cuesta $  200 y se incurren un costo de$100 por hora cada vez que se desconecta el equipo.
-                <br> <br>  
+                <br> <br>
                   </p>
                 <p>
-                   Determine cuál de las dos políticas anteriores es más económica 
+                   Determine cuál de las dos políticas anteriores es más económica
                    (Simule la operación del equipo  durante 20.000 horas).
                 </p>
                     <div class="button-group">
                     <button type="button" class="gradient-button3" onclick="window.location.href='{{ route('ejercicio2.historial') }}'">Ver historial</button>
                     <button type="button" class="gradient-button2" onclick="window.location.href='{{ route('ejercicio2.simular') }}'">Simular</button>
-                     
+
                     </div>
             </div>
-            
+
         </div>
-        
+
     </main>
 
     @push('css')
         <style>
              .gradient-button {
-                background: transparent; 
-                color: #EE7983;  
-                border: 1px solid #FFD700;  
-                border-radius: 30px;  
-                padding: 10px 15px; 
-                font-size: 16px;  
-                cursor: pointer;  
-                transition: background 0.3s ease, color 0.3s ease;  
-                margin-right: 25px;  
+                background: transparent;
+                color: #EE7983;
+                border: 1px solid #FFD700;
+                border-radius: 30px;
+                padding: 10px 15px;
+                font-size: 16px;
+                cursor: pointer;
+                transition: background 0.3s ease, color 0.3s ease;
+                margin-right: 25px;
             }
             .gradient-button3 {
-                background: transparent; 
+                background: transparent;
                 border: 1px solid white;
                 border-radius: 30px;
-                padding: 10px 65px; 
-                font-size: 15px; 
-                color: white; 
+                padding: 10px 65px;
+                font-size: 15px;
+                color: white;
                 margin-left: 5px;
             }
             .gradient-button:hover{
-                background: linear-gradient(to right, #FF4500, #FFD700);  
-                color: white;  
+                background: linear-gradient(to right, #FF4500, #FFD700);
+                color: white;
             }
              .gradient-button2 {
-                border: none;  
-                border-radius: 30px;  
-                padding: 10px 75px; 
-                font-size: 15px;   
+                border: none;
+                border-radius: 30px;
+                padding: 10px 75px;
+                font-size: 15px;
                 background: linear-gradient(to right, #F6CA45, #EE7983);
             }
             .container-fluid {
@@ -104,16 +104,16 @@
                 justify-content: flex-end;
                 gap: 5px;
             }
-            
+
             @media (max-width: 768px) {
                 .gradient-button {
-                    font-size: 14px;  
-                    padding: 8px 16px;  
+                    font-size: 14px;
+                    padding: 8px 16px;
                 }
             .text-justify {
                 text-align: justify;
             }
-    
+
             p {
                 margin-bottom: 3rem;
             }
